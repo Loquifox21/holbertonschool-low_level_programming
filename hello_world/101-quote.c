d96edd18326c2c3ae389e5eeea11306f77bf2209
Loquifox21 <lukasgarridobak@gmail.com>
2022-06-06 09:39:54 -0700

    101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
- * main - whatever
+ * main - programm
  * Return: 1
  */
 
@@ -11,5 +11,5 @@ int main(void)
 	
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 21);
 	
-	return (1)
+	return (1);
 } 

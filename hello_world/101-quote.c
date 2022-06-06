b964b817f604acc370c7b07fb3fe46542f723fd9
Loquifox21 <lukasgarridobak@gmail.com>
2022-06-06 09:45:55 -0700

    101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -8,8 +8,7 @@
 
 int main(void) 
 {
-	
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 35);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 60);
 	
 	return (1);
 } 

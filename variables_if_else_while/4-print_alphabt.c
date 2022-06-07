07767e198e1e3691a21d28b01bffebe330673d90
Loquifox21 <lukasgarridobak@gmail.com>
2022-06-07 13:37:49 -0700

    4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -6,20 +6,12 @@
 int main(void)
 {
 	char c;
-	char e;
 	char q;
+	char e;
 
 	for (c = 'a'; c <= 'z'; c++) 
 	{
 	putchar(c);
-	}
-	for (e = 'e'; e--)
-	{
-	putchar(e)
-	}
-	for (q = 'q'; q--)
-	{
-	putchar(q)
 	}
 		
 	putchar('\n');

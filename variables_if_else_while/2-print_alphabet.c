ddf6305be88f478cdf627e21ac62ee141ec371fc
Loquifox21 <lukasgarridobak@gmail.com>
2022-06-07 09:28:53 -0700

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet
+ * Return: 0
+ */
+int main()
+{ 
+	char a = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,,r,s,t,u,v,w,x,y,z';
+	putchar(a);
+	return (0);
+}

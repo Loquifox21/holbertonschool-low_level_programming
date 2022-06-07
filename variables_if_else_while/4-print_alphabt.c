3372344926a88c4d4c3f6a3358939492f51d2eed
Loquifox21 <lukasgarridobak@gmail.com>
2022-06-07 10:24:30 -0700

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	char e;
+	char q;
+
+	for (c = 'a'; c <= 'z'; c++) 
+	{
+	putchar(c);
+	}
+	for (e = 'e'; e--)
+	{
+	putchar(e)
+	}
+	for (q = 'q'; q--)
+	{
+	putchar(q)
+	}
+		
+	putchar('\n');
+	return (0);
+
+}

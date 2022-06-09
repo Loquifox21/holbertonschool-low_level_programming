222c5a66fcb443cd1a49f7b4639eafc54a917e1b
Loquifox21 <lukasgarridobak@gmail.com>
2022-06-09 10:54:42 -0700

    5-sign.c

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_sign - prints the alphabet
+ * @n : skere
+ * Return: 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}

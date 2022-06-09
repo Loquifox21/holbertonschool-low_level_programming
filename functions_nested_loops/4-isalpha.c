b4413ab274ac387b9452a5aa287e896ae3537ab5
Loquifox21 <lukasgarridobak@gmail.com>
2022-06-09 10:23:54 -0700

    4-isalpha.c

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,17 +1,17 @@
 #include <ctype.h>
 #include "main.h"
 /**
- * _islower - prints the alphabet
+ * _isalpha - prints the alphabet
  * @c : skere
  * Return: 0
  */
 
-int _islower(int c)
+int _isalpha(int c)
 {
 	if (c >= 64 && c <= 90)
 		return (1);
 	if (c >= 97 && c <= 122)
-		return (1)
+		return (1);
 	else
 		return (0);
 }

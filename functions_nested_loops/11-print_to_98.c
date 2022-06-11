bac476e658ee3c92da6d910ecfda7586355b1ea6
Loquifox21 <lukasgarridobak@gmail.com>
2022-06-10 18:54:44 -0700

    Task 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (n  = n; n <= 98; n++)
+			printf("%d, ", n);
+	}
+	else
+		for (n = n; n > 98; n--)
+			printf("%d, ", n);
+			
+	printf("98\n");
+}
